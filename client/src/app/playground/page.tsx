0834dc961ef9d7d067b96b3dc562ec208fda9252
Kevin Wu <kevinwu098@gmail.com>
2024-10-20 03:39:02 -0700

    avoidance

--- client/src/app/playground/page.tsx
+++ client/src/app/playground/page.tsx
@@ -1,9 +1,14 @@
+import { HereMap } from "@/components/dashboard/here/map";
 import { SplatViewer } from "@/components/splat/splat-viewer";
 
 export default function Playground() {
     return (
-        <div className="h-full">
-            <SplatViewer splat="foo" />
+        // <div className="h-full">
+        //     <SplatViewer splat="foo" />
+        // </div>
+
+        <div className="h-[100dvh] min-h-[100dvh] w-full">
+            <HereMap apikey={process.env.NEXT_PUBLIC_HERE_KEY} />
         </div>
     );
 }
